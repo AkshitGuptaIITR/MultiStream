945ae3f1f3c2afe96ae678b5a30a327c74624315
Akshit <Gakshit92@gmail.com>
2023-02-20 19:31:39 +0530

    adding react roites

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Video from './Components/Video';
+import { Route, Router } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path='/' element={<Login />} />
+    </Router>
   );
 }
 
---
+++ src/Components/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Webcam from 'react-webcam'
+
+const Video = () => {
+  return (
+    <div>
+      <Webcam imageSmoothing={true} />
+    </div>
+  )
+}
+
+export default Video
